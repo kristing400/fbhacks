754c8e8b291ba4bb736e89ac3ce5a5cb5829b2ba
christinaou <christinaou@cmu.edu>
2018-11-16 02:29:17 -0800

    making viewProfile

--- createProfile/urls.py
+++ createProfile/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('getKeywords/', views.getKeywords),
-    path('editProfiles/', views.editProfiles)
+    path('viewProfile/', views.viewProfile)
 ]
