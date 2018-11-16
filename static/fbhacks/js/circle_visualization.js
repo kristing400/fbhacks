e388120b8e3ef5206f02c415696408779c7a87cb
Kristin Yin <kristinyin@hotmail.com>
2018-11-16 08:38:14 -0800

    bubble visualization connected

--- static/fbhacks/js/circle_visualization.js
+++ static/fbhacks/js/circle_visualization.js
@@ -22,12 +22,12 @@ function distance(x1, y1, x2, y2) {return Math.sqrt( Math.pow((x1-x2), 2) + Math
 
 
 function markGrid(grid, diam, x, y) {
-	var topx = x - (diam / 2);
-	var topy = y - (diam / 2);
+	var topx = x - int(diam / 2);
+	var topy = y - int(diam / 2);
 
 	for (var i = 0; i < diam; i++) {
 		for (var j = 0; j < diam; j++){
-			if (distance(i+topx,j+topy, x, y) <= (diam/2)){
+			if (distance(i+topx,j+topy, x, y) <= int(diam/2)){
 
 				// console.log(i+topx);
 				// console.log(j+topy);
@@ -42,12 +42,12 @@ function doesItFitGrid(grid, diam, x, y) {
 	if (x < (diam / 2)) {return false};
 	if (y < (diam / 2)) {return false};
 
-	var topx = x - (diam / 2);
-	var topy = y - (diam / 2);
+	var topx = x - int(diam / 2);
+	var topy = y - int(diam / 2);
 
 	for (var i = 0; i < diam; i++) {
 		for (var j = 0; j < diam; j++){
-			if (distance(i+topx,j+topy, x, y) <= (diam/2)){
+			if (distance(i+topx,j+topy, x, y) <= int(diam/2)){
 				if (i+topx >= 400) {return false};
 				if (j+topy >= 400) {return false};
 				if (grid[i+topx][j+topy] == 1) {return false};
@@ -62,6 +62,12 @@ function setup() {
 	var height = 400;
 	var canvas = createCanvas(width, height);
 	noLoop();
+	var keywords = document.getElementById("p5-canvas").getAttribute("value");
+
+	var sals = document.getElementById("info").getAttribute("value");
+	data = {};
+	console.log(saliences);
+
 	canvas.parent('p5-canvas');
 
 	var grid = new int(400);
@@ -75,14 +81,26 @@ function setup() {
 			grid[x][y] = 0;
 		}
 	}
+	var words = keywords.split(", ");
+	var saliences = sals.split(", ");
+	for (var k = 0; k < words.length; k++) {
+		if (words[k][0] == "[") {
+			words[k] = words[k].substring(2, words[k].length-1);
+			saliences[k] = parseFloat(saliences[k].substring(2, saliences[k].length-1));
+		} else if (words[k][words.length-1] == "]") {
+			words[k] = words[k].substring(1, words[k].length-2);
+			saliences[k] = parseFloat(saliences[k].substring(1, saliences[k].length-2));
+		} else {
+			words[k] = words[k].substring(1, words[k].length-1);
+			saliences[k] = parseFloat(saliences[k].substring(1, saliences[k].length-1));
+		}
+	}
+	console.log(words);
+	console.log(saliences);
 
-
-	for (var key in data) {
-		console.log(key);
-		console.log(data[key]);
-
-		var diam = (data[key]) * 100+40;
-
+	for (var j = 0; j < words.length; j++) {
+		var diam = (saliences[j] * 300+50);
+		console.log(words[j], diam);
 		function addCircle(){
 			for (var y = 100; y < 350; y++) {
 				for (var x = 100; x < 350; x++){
@@ -92,7 +110,7 @@ function setup() {
 							x: x,
 							y: y,
 							diam: diam,
-							word: key
+							word: words[j]
 						});
 						return
 					}
@@ -100,6 +118,7 @@ function setup() {
 			}
 		}
 		addCircle();
+		console.log(words[j]);
 	}
 }
 
